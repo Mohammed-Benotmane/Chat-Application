968ee46198e31ce3d469326e577a8613666d6f8f
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2019-07-14 20:07:30 +0200

    add MessageAdapter Class

--- app/src/main/java/com/jetlight/myappchat/MessageAdapter.java
+++ app/src/main/java/com/jetlight/myappchat/MessageAdapter.java
@@ -1,9 +1,13 @@
 package com.jetlight.myappchat;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -18,6 +22,29 @@ public class MessageAdapter extends ArrayAdapter<Message> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        if(convertView == null){
+            convertView =((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent, false);
+        }
+        ImageView photoImageView = (ImageView) convertView.findViewById(R.id.photoImageView);
+        TextView messageTextView = (TextView) convertView.findViewById(R.id.messageTextView);
+        TextView authorTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+
+        Message message = getItem(position);
+
+        boolean isPhoto = message.getPhotoURL() != null;
+        if (isPhoto) {
+            messageTextView.setVisibility(View.GONE);
+            photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(photoImageView.getContext())
+                    .load(message.getPhotoURL())
+                    .into(photoImageView);
+        } else {
+            messageTextView.setVisibility(View.VISIBLE);
+            photoImageView.setVisibility(View.GONE);
+            messageTextView.setText(message.getMessage());
+        }
+        authorTextView.setText(message.getTitre());
+
+        return convertView;
     }
 }

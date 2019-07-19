9c046806f808acff505d087bc7c8f1a81d7fc387
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2019-07-19 14:08:08 +0200

    add childEventListener

--- app/src/main/java/com/jetlight/myappchat/MainActivity.java
+++ app/src/main/java/com/jetlight/myappchat/MainActivity.java
@@ -14,12 +14,17 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends Activity {
@@ -37,6 +42,7 @@ public class MainActivity extends Activity {
     private Button mSendButton;
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
+    private ChildEventListener childEventListener;
 
     private String mUsername;
 
@@ -99,7 +105,34 @@ public class MainActivity extends Activity {
             }
         });
         mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
+        childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                dataSnapshot.getValue(Message.class);
+            }
 
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+        databaseReference.addChildEventListener(childEventListener);
 
 
     }
